af7c0087125e18dc0c436236506bcdd3c54fe6e9
Fellipe Guimaraes <freador@gmail.com>
2020-04-22 07:40:04 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
-
-
+import Header from  './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage}/>
+        <Route path="/signin" component={SignInAndSignUpPage}/>
       </Switch>
       
     </div>
